a9f9e9327bdf58d4916b2e1fe7dd488605eefd33
Tahir Ali Khan <sigmaprofile@hotmail.com>
2022-01-26 20:25:13 +0500

    session3 local

--- zuul-api-gateway/src/main/java/com/vrooziks/zuulapigateway/ZuulApiGatewayApplication.java
+++ zuul-api-gateway/src/main/java/com/vrooziks/zuulapigateway/ZuulApiGatewayApplication.java
@@ -2,8 +2,9 @@ package com.vrooziks.zuulapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-
+@EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulApiGatewayApplication {
